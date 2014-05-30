e59393009ab51801bd35aa74f9b224b473a8f52a
someben <ben@somethingmodern.com>
2014-05-30 14:49:23 -0700

    cleanup & license headers

--- src/skream/core_srv_rest.clj
+++ src/skream/core_srv_rest.clj
@@ -1,3 +1,11 @@
+;
+; (C) Copyright 2014 Ben Gimpert (ben@somethingmodern.com)
+;
+; All rights reserved. This program and the accompanying materials
+; are made available under the terms of the Eclipse Public License v1.0
+; which accompanies this distribution, and is available at
+; http://www.eclipse.org/legal/epl-v10.html
+;
 (ns skream.core-srv-rest
   (:use (ring.adapter jetty)
         (compojure core))
